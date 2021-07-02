8defac1109e463c342364042b3c9a40fea83211c
russiancoin <cpws@mail.ru>
2021-07-02 09:26:04 +0300

    Added Menu with items

--- main.py
+++ main.py
@@ -41,13 +41,13 @@ KV = '''
             source: "data/logo/kivy-icon-256.png"
 
     MDLabel:
-        text: "KivyMD library"
+        text: app.title
         font_style: "Button"
         size_hint_y: None
         height: self.texture_size[1]
 
     MDLabel:
-        text: "kivydevelopment@gmail.com"
+        text: app.by_who
         font_style: "Caption"
         size_hint_y: None
         height: self.texture_size[1]
@@ -71,12 +71,21 @@ Screen:
                     orientation: 'vertical'
 
                     MDToolbar:
-                        title: "Mortgage Calculator"
+                        title: app.title
                         elevation: 10
                         left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
+                        right_action_items: [["star-outline", lambda x: app.on_star_click()]]
+                        md_bg_color: 0, 0, 0, 1
 
                     MDTabs:
                         id: tabs
+                        on_tab_switch: app.on_tab_switch(*args)
+                        size_hint_y: None
+                        height: "48dp"
+                        tab_indicator_anim: False
+                        background_color: 0.1, 0.1, 0.1, 1
+
+                    Widget:
 
 
         MDNavigationDrawer:
@@ -86,8 +95,6 @@ Screen:
                 id: content_drawer
 '''
 
-class Tab(MDFloatLayout, MDTabsBase):
-    pass
 
 class ContentNavigationDrawer(BoxLayout):
     pass
@@ -110,31 +117,58 @@ class DrawerList(ThemableBehavior, MDList):
         instance_item.text_color = self.theme_cls.primary_color
 
 
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
+
+
 class MortgageCalculatorApp(MDApp):
+    title = "Mortgage Calculator"
+    by_who = "author Oleg Shpagin"
+
     def build(self):
         return Builder.load_string(KV)
 
     def on_start(self):
-        icons_item = {
-            "folder": "My files",
-            "account-multiple": "Shared with me",
-            "star": "Starred",
-            "history": "Recent",
-            "checkbox-marked": "Shared with me",
-            "upload": "Upload",
+        icons_item_menu_lines = {
+            "account-cowboy-hat": "About author",
+            "youtube": "My YouTube",
+            "coffee": "Donate author",
+            "github": "Source code",
+            "share-variant": "Share app",  # air-horn
+            "shield-sun": "Dark/Light",
         }
-        for icon_name in icons_item.keys():
+        icons_item_menu_tabs = {
+            "calculator-variant": "Input",  # ab-testing
+            "table-large": "Table",
+            "chart-areaspline": "Graph",
+            "chart-pie": "Chart",  # chart-arc
+            "book-open-variant": "Sum",
+        }
+        for icon_name in icons_item_menu_lines.keys():
             self.root.ids.content_drawer.ids.md_list.add_widget(
-                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+                ItemDrawer(icon=icon_name, text=icons_item_menu_lines[icon_name])
             )
-        # for name_tab in list(md_icons.keys())[15:30]:
-        #     self.root.ids.tabs.add_widget(Tab(icon=name_tab, title=name_tab))
 
-        for icon_name, name_tab in icons_item.items():
+        for icon_name, name_tab in icons_item_menu_tabs.items():
             self.root.ids.tabs.add_widget(
-                Tab(text=f"[ref={name_tab}][font={fonts[-1]['fn_regular']}]{md_icons[icon_name]}[/font][/ref] {name_tab}")
+                Tab(
+                    text=f"[size=20][font={fonts[-1]['fn_regular']}]{md_icons[icon_name]}[/size][/font] {name_tab}"
+                )
             )
 
+    def on_tab_switch(self, instance_tabs, instance_tab, instance_tab_label, tab_text):
+        '''Called when switching tabs.
+
+        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
+        :param instance_tab: <__main__.Tab object>;
+        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
+        :param tab_text: text or name icon of tab;
+        '''
+
+        print("tab clicked! " + tab_text)
+
+    def on_star_click(self):
+        print("star clicked!")
 
 
 MortgageCalculatorApp().run()
